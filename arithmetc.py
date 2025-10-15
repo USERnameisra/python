a4a34de75c4ead0309fd0b7b5f1e4307d74ca274
USERnameisra <markisrael099@icloud.com>
2025-10-16 00:51:15 +0800

    Add main.py

---
+++ arithmetc.py
@@ -0,0 +1,44 @@
+import math
+
+
+
+#x = 3.14
+#y = 5
+#z= 6
+
+# result = round(x)
+# result = abs(y)
+#result= pow(4,3)
+#result = max(x,y,z)
+#result = min(x,y,z)
+
+
+#print(math.pi)
+#result = math.sqrt(z)
+
+#print(result)
+
+
+#radius = float(input("enter radius:" ))
+
+#c = 2 * math.pi * radius
+
+
+#print(round(c,2))
+
+
+#radius =  float(input("ente radius of circle: "))
+
+
+#area = math.pi * pow(radius, 2)
+
+#print(round(area,2))
+
+
+#a = float(input("enter a: "))
+#b = float(input("enter b: "))
+
+#c = math.sqrt(pow(a,2)+ pow(b,2))
+
+#print(f"side of c = {c}")
+
