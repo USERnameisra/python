a78aec5eecf51ab082afef3f96a70d023be9057d
USERnameisra <markisrael099@icloud.com>
2025-10-18 22:45:46 +0800

    update

---
+++ string.py
@@ -0,0 +1,13 @@
+
+phone_num = input("enter number: ")
+#result = len(name)
+#result = name.find("a")
+#result = name.rfind("o")
+#result = name.capitalize()
+#result = name.lower()
+#result = name.isdigit()
+#esult = name.isalpha()
+#result = phone_num.count("-")
+#phone_num = phone_num.replace("-"," ")
+
+print(phone_num)
