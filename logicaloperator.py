9fb8dcb86569c2c6708a21eb32bd013c9e41cc0a
USERnameisra <markisrael099@icloud.com>
2025-10-18 21:44:24 +0800

    update

---
+++ logicaloperator.py
@@ -0,0 +1,14 @@
+#conditoional expression
+
+
+
+num = 5
+a = 6
+b = 7
+
+#print("valid"if num > 18 else "young" )
+#print("EVEN"if num % 2 == 0 else "ODD")
+#max_num = a if a>b else b
+#min_num = a if a<b else b
+#print(max_num)
+#print(min_num)
